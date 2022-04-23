98f4d021cc1016689a271b773b2e6e386478986b
RensWest <94118091+RensWest@users.noreply.github.com>
2022-04-23 18:54:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("test");
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("nav ul");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll("nav ul li a").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
